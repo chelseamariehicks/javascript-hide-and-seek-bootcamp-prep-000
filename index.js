ae6b700e3ec656bcd4733b41c46cf81971cc8d4e
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-12-05 17:08:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested.target')
+}
+
+function increaseRankBy(n){
+var rankedLists = document.querySelectorAll('.ranked-list')
+let children = firstList.children
+
+for(let i=0, l = children.length; i <l; i++) {}
+}
+
+function deepestChild() {
+console.log(deepestChild().innerHTML)
+
+}
